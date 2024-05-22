8da27b94877bd467e2faf0fc664a032e97b4685f
hazem lachheb <lachhebhazem87@gmail.com>
2024-05-22 23:52:08 +0100

    modal service nettoyage

--- src/app/servicenettoyage/servicenettoyage.component.ts
+++ src/app/servicenettoyage/servicenettoyage.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Utilisateur } from '../Entites/Utilisateur.Entites';
 import { CrudService } from '../service/crud.service';
 import { Planning } from '../Entites/Planning.Entites';
-import { forkJoin } from 'rxjs'; // Importer forkJoin
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-servicenettoyage',
@@ -13,36 +13,27 @@ export class ServicenettoyageComponent implements OnInit {
   listeplanning: Planning[] = [];
   listeutilisateur: Utilisateur[] = [];
   user: Utilisateur[] = [];
+  selectedUtilisateur: Utilisateur | null = null; // Pour stocker l'utilisateur sélectionné
 
   constructor(private crudService: CrudService) { }
 
   ngOnInit(): void {
-    console.log("hatha utilisateur  :", this.listeutilisateur);
-    console.log("hatha list planning:", this.listeplanning);
-
     this.crudService.getPlanning().subscribe((planning: Planning[]) => {
       this.listeplanning = planning;
-   
-      // Créer un tableau d'observables pour récupérer les listeutilisateur associés aux plannings
-      const observables = this.listeplanning.map(i =>
-        this.crudService.getUtilisateurByPlanning(i.id));
-      // Utiliser forkJoin pour attendre que toutes les requêtes soient terminées
+      const observables = this.listeplanning.map(i => this.crudService.getUtilisateurByPlanning(i.id));
       forkJoin(observables).subscribe(results => {
         this.listeutilisateur = results;
-        console.log("Liste des listeutilisateur après ajout :", this.listeutilisateur);
       });
-      
     });
-  
-
-    console.log("hatha list planning après chargement:", this.listeplanning);
-    
 
     this.getUtilisateursParRole('Femme de menage');
   }
 
   getUtilisateursParRole(role: string): void {
-    this.crudService.getUtilisateursParRole(role)
-      .subscribe(user => this.user = user);
+    this.crudService.getUtilisateursParRole(role).subscribe(user => this.user = user);
+  }
+
+  selectfemme(utilisateur: Utilisateur): void {
+    this.selectedUtilisateur = utilisateur; // Définir l'utilisateur sélectionné
   }
 }
