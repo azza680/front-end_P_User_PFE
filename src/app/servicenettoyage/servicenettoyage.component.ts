98d36fa9ed417c18b645dc8640b66ee7c1794173
hazem lachheb <lachhebhazem87@gmail.com>
2024-05-29 12:12:10 +0100

    bloque le reservation de planning lorsque l'utilisateur non connecter

--- src/app/servicenettoyage/servicenettoyage.component.ts
+++ src/app/servicenettoyage/servicenettoyage.component.ts
@@ -21,11 +21,14 @@ export class ServicenettoyageComponent implements OnInit {
   selectedPlanning: Planning | null = null; // Nouvelle propriété pour le planning sélectionné
   searchQuery: string = '';
   activefemme: string = '';
+  IsloggedIn: boolean;
   paymentHandler: any = null;
 
   constructor(private crudService: CrudService, private route: Router, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.IsloggedIn = this.crudService.isLoggedIn(); // Utilisez crudService au lieu de service
+
     this.crudService.getPlanning().subscribe((planning: Planning[]) => {
       this.listeplanning = planning;
       const observables = this.listeplanning.map(i => this.crudService.getUtilisateurByPlanning(i.id));
@@ -98,6 +101,11 @@ export class ServicenettoyageComponent implements OnInit {
   }
 
   reserver(rq: ReservationFM) {
+    if (!this.IsloggedIn) {
+      this.messageCommande = `<div class="alert alert-warning" role="alert">Vous devez être connecté pour réserver.</div>`;
+      return;
+    }
+
     this.messageCommande = `<div class="alert alert-primary" role="alert">Veuillez patienter ...</div>`;
     let datas = this.crudService.getUserInfo(); // Utilisation de crudService au lieu de service
 
@@ -120,6 +128,11 @@ export class ServicenettoyageComponent implements OnInit {
   }
 
   makePayment(amount: any) {
+    if (!this.IsloggedIn) {
+      this.messageCommande = `<div class="alert alert-warning" role="alert">Vous devez être connecté pour effectuer un paiement.</div>`;
+      return;
+    }
+
     const paymentHandler = (<any>window).StripeCheckout.configure({
       key: 'pk_test_51PFI24F29zVOYaoLNwA55lQnMETgMsgILXooIySTysEtaUYck09EzbfHklFnfQQm2zmmtZam1Ss796gimwKUNUv4006HgVUZXa',
       locale: 'auto',
